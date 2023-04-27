6c0f2563e64b2bf9adaf0ad2e6870e0d2a23206b
Ecaterina <barganecaterina@yahoo.com>
2023-04-27 23:56:51 +0300

    built in functions

---
+++ internal functions/built-in.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$string = "Hello World!";
+// echo strlen($string);
+// echo strpos($string, "o");
+// echo str_replace("World!", "Ecaterina", $string )
+// echo strtolower($string);
+// echo strtoupper($string);
+// echo substr($string, 2, 2);
+// echo substr($string, 2, -2);
+print_r(explode(" ", $string)); 
+
+?>
+  
+</body>
+</html>
